ba3b46ca9d21f0c37ea63a758b8b134e7ca2a06d
Arthur <kingart808@gmail.com>
2019-06-18 12:20:25 -1000

    on number 4

--- js/app.js
+++ js/app.js
@@ -1,17 +1,51 @@
 // 1. Add an event to the 'See Details' content that will create an alert box that says 'Not Available in Hawaii.' after clicking on See Details.
+let see = document.querySelector('#details')
+function notice() {
+    alert('Not Avalible in Hawaii.')
+}
+see.addEventListener('click',notice)
 
 //2. Add an event to the div element with the id of 'name1' that will show/hide the description ('descrip1') after hovering over Air Jordan II.
-
+let hover = document.querySelector('#name1')
+let des = document.querySelector('#descrip1')
+hover.addEventListener('mouseover', function(event){
+    des.style.display = 'block'
+})
+hover.addEventListener('mouseleave', function(event){
+    des.style.display = 'none'
+})
 //3. Add an event to the div element with the id of 'name2' that will show/hide the following description after clicking on the element.
 //'The instantly recognizable Jumpman silhouette made its debut with the Air Jordan 3 during Michael Jordan's 1987-88 NBA season.'
+let name = document.querySelector('#name2')
+name.addEventListener('click',function(event){
+    if(newDiv.style.display === 'none'){
+    newDiv.style.display = 'block'
+    }else{
+        newDiv.style.display = 'none'
+    }
+    
+})
 
 
 //You'll need to:
 // create a div element with an id of 'descrip2'
 //append this element to the div element with the id of 'name2' 
-
+var newDiv = document.createElement('div')
+newDiv.id = 'descrip2'
+newDiv.innerHTML = "The instantly recognizable Jumpman silhouette made its debut with the Air Jordan 3 during Michael Jordan's 1987-88 NBA season."
+name.appendChild(newDiv)
 //4. Add events to the all the thumbs up icon that will add a count for each time the icon is clicked on for any shoe.
-
+var thumb = document.querySelectorAll('#thumbs')
+for (let i = 0; i < thumb.length; i++) {
+    thumb[i].onclick = count++
+    
+}
+var count = document.createElement('div');
+count.innerHTML = 0
+console.log(thumb)
+// thumb.addEventListener('click',function(){
+//     count++
+// })
 //5. Add an event to the plus icon that will increment the price of the Air Jordan V shoe each time the icon is clicked on.
 
 //6. Add an event to the minus icon that will decrement the price of the Air Jordan VI shoe each time the icon is clicked on.
